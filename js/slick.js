f83b434b2d5cd7ee35a8ec73f3c7d9c86d085a23
ownage <118118977+ownagge@users.noreply.github.com>
2023-08-03 18:05:27 +0300

    Create slick.js

---
+++ js/slick.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('.rev__slider').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: true,
+	  dots: false,
+	  autoplay: true,
+	  autoplaySpeed: 900,
+	  speed: 3000
+	})
+  });
